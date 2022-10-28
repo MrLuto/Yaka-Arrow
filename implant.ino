20329a58d91fc1fff38cfdbf3a949c1b9598bd67
MrLuto <mmudde2@gmail.com>
2022-10-28 17:15:44 +0200

    Changes to be committed: 	new file:   implant.ino

---
+++ implant.ino
@@ -0,0 +1,49 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+const char* ssid = "SSID";
+const char* password = "PASSWORD";
+String serverName = "GET REQUEST URL";
+
+void setup() {
+  Serial.begin(115200); 
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+  digitalWrite(0, HIGH);
+}
+
+void loop() {
+  if (digitalRead(0) == LOW) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      WiFiClient client;
+      HTTPClient http;
+      http.begin(client, serverName);
+      int httpResponseCode = http.GET();
+      
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+  }
+}
